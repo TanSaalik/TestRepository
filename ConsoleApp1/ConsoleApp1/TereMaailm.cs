7a8e2fdb47ff1d9291ce9747e04ef4c562e3339b
opilane <opilane@HP-CZC6297B9P>
2018-04-10 18:40:34 +0300

    Push Visual Studiost

--- ConsoleApp1/ConsoleApp1/TereMaailm.cs
+++ ConsoleApp1/ConsoleApp1/TereMaailm.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Tere maailm!");
+            Console.WriteLine("Nyyd on 2kki push ka.");
             Console.ReadLine();
         }
     }
