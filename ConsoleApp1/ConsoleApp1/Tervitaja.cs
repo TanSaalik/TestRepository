5b5bd32ad5984a91d97e25da51dbd70c8374b211
opilane <opilane@HP-CZC6297B9P>
2018-04-10 18:54:07 +0300

    Tervitaja klass

---
+++ ConsoleApp1/ConsoleApp1/Tervitaja.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Tervitaja
+    {
+        internal static void Tervita(string keda)
+        {
+            Console.WriteLine("Tere" + keda + "!");
+        }
+    }
+}
